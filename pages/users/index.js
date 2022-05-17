1fd6f09a10762422cfd00293f6a6efa4247078f8
Dion Singh <55777067+DionMS8@users.noreply.github.com>
2022-05-17 06:21:10 -0400

    Creating Dynamic Routes based on User IDs

--- pages/users/index.js
+++ pages/users/index.js
@@ -1,28 +1,31 @@
-import styles from '../styles/Users.module.css';
+import Link from 'next/link';
+import styles from '../../styles/Users.module.css';
 
-//=====[FETCHING THE JSON DATA]========================================================================
+//=====[FETCHING THE JSON DATA]================================================================================
 
 export const getStaticProps = async () => {
   const response = await fetch('https://jsonplaceholder.typicode.com/users');
   const data = await response.json();
 
   return {
-    props: { users: data },
+    props: {
+      users: data,
+    },
   };
 };
 
-//=====[CREATING THE USERS COMPONENT]==================================================================
+//=====[CREATING THE USERS COMPONENT]=========================================================================
 
 const Users = ({ users }) => {
   return (
     <div>
       <h1>List of Users</h1>
       {users.map((user) => (
-        <div key={user.id}>
-          <a>
+        <Link href={`/users/${user.id}`} key={user.id}>
+          <a className={styles.single}>
             <h3>{user.name}</h3>
           </a>
-        </div>
+        </Link>
       ))}
     </div>
   );
