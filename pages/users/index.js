68098f4db19bf06147629e0aac7869ee607fd9e3
Dion Singh <55777067+DionMS8@users.noreply.github.com>
2022-05-17 00:52:47 -0400

    Fetching JSON Data using getStaticProps( )

--- pages/users/index.js
+++ pages/users/index.js
@@ -1,9 +1,29 @@
-// import styles from '../styles/Users.module.css';
+import styles from '../styles/Users.module.css';
 
-const Users = () => {
+//=====[FETCHING THE JSON DATA]========================================================================
+
+export const getStaticProps = async () => {
+  const response = await fetch('https://jsonplaceholder.typicode.com/users');
+  const data = await response.json();
+
+  return {
+    props: { users: data },
+  };
+};
+
+//=====[CREATING THE USERS COMPONENT]==================================================================
+
+const Users = ({ users }) => {
   return (
     <div>
       <h1>List of Users</h1>
+      {users.map((user) => (
+        <div key={user.id}>
+          <a>
+            <h3>{user.name}</h3>
+          </a>
+        </div>
+      ))}
     </div>
   );
 };
